7212cbb0553792611184d2a209c2b8be8943b0ee
Garieldas <dainiusrainys99@gmail.com>
2025-09-21 22:21:04 +0300

    Refactor HomePageView to wrap buttons in router-links for navigation.

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -17,20 +17,25 @@
 
         <div class="relative h-48 sm:h-64 mb-8">
           <div class="absolute inset-0 flex items-center justify-center">
+            <router-link :to="{name:  'pokemons'}">
             <img src="../assets/images/bulb.png" alt="Pikachu" class="w-24 sm:w-32 animate-bounce"/>
+            </router-link>
           </div>
         </div>
         <div class="flex flex-col md:flex-row justify-center gap-4 sm:gap-8">
-          <button class="group relative overflow-hidden rounded-2xl backdrop-blur-md bg-white/30 border-2 border-white/50 hover:bg-white/40 transition-all duration-300 p-4 sm:p-8">
+        <router-link :to="{name:  'pokemons'}">
+          <button class="group relative overflow-hidden rounded-2xl backdrop-blur-md bg-white/30 border-2 border-white/50 hover:bg-white/40 transition-all duration-300 p-4 sm:p-8 cursor-pointer">
             <div class="relative z-10">
               <img src="../assets/images/pikachu_home_gif.webp" alt="Pikachu" class="w-24 h-24 sm:w-32 sm:h-32 mx-auto pixelated mb-4 group-hover:scale-110 transition-transform"/>
               <p class="text-lg sm:text-xl font-bold text-gray-800 text-center drop-shadow-lg">
-                Search pokemons!
+                Search Pokemons!
               </p>
             </div>
           </button>
+          </router-link>
 
-          <button class="group relative overflow-hidden rounded-2xl backdrop-blur-md bg-white/30 border-2 border-white/50 hover:bg-white/40 transition-all duration-300 p-4 sm:p-8">
+        <router-link :to="{name:  'favorite-pokemons'}">
+          <button class="group relative overflow-hidden rounded-2xl backdrop-blur-md bg-white/30 border-2 border-white/50 hover:bg-white/40 transition-all duration-300 p-4 sm:p-8 cursor-pointer">
             <div class="relative z-10">
               <img src="../assets/images/favorite-image.gif" alt="Eevee" class="w-24 h-24 sm:w-32 sm:h-32 mx-auto pixelated mb-4 group-hover:scale-110 transition-transform"/>
               <p class="text-lg sm:text-xl font-bold text-gray-800 text-center drop-shadow-lg">
@@ -38,6 +43,7 @@
               </p>
             </div>
           </button>
+          </router-link>
         </div>
       </div>
     </main>

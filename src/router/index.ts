8d06965d4ee920a6d6858acaf1b51d7aa1eaebc0
Garieldas <dainiusrainys99@gmail.com>
2025-09-21 21:58:59 +0300

    Created routes. Added router view display.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePageView from '@/views/HomePageView.vue'
+import PokemonsView from '@/views/PokemonsView.vue'
+import PokemonDetailsView from '@/views/PokemonDetailsView.vue'
+import FavoritePokemonsView from '@/views/FavoritePokemonsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      children: [
+        {
+          path: "",
+          name: "home",
+          component: HomePageView,
+        }
+      ]
+    },
+    {
+      path: "/pokemons",
+      children: [
+        {
+          path: "",
+          name: "pokemons",
+          component: PokemonsView
+        },
+        {
+          path: ":id",
+          name: "pokemon-details",
+          component: PokemonDetailsView,
+          props: true,
+        }
+      ]
+    },
+    {
+      path: "/favorite-pokemons",
+      name: "favorite-pokemons",
+      component: FavoritePokemonsView
+    },
+  ]
 })
 
 export default router
